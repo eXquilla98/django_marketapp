014ff2f69c8f6049e137933976fa27945c67e8aa
exquilla98 <dinukamalshan14@gmail.com>
2024-09-28 00:14:44 +0530

    2nd commit miss yeasterday

--- serverstart/serverstart/urls.py
+++ serverstart/serverstart/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from myapp.views import index
 urlpatterns = [
-    path('', index, name='index'),
+
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
